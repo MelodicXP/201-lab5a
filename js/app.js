19e35bd7b01fc5c6d100432dd269843e5cd52b90
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-05-20 10:42:47 -0500

    Add function to pass question five

--- js/app.js
+++ js/app.js
@@ -107,11 +107,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let productOne = multiply(multArr[0], multArr[1])[0];
+  let productTwo = multiply(productOne, multArr[2])[0];
 
+  return [productTwo , `The numbers ${multArr} have a product of ${productTwo}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
